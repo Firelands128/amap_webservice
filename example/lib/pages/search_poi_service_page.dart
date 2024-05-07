ce7788f37c6c9c52b5d9787476d5f72e2fa4528f
Wenqi Li <firelands128@gmail.com>
2024-05-07 23:24:18 +0800

    add search around api service and update the according example page

--- example/lib/pages/search_poi_service_page.dart
+++ example/lib/pages/search_poi_service_page.dart
@@ -20,13 +20,26 @@ class _SearchPoiServicePageState extends State<SearchPoiServicePage> {
     apiKey: '0e2f6cd577c7b01f2f10e8a8a4cdf153',
     secretKey: '36b5528aecd3e4aba379e1ef352820fd',
   );
-  final geoTextController = TextEditingController();
-  final geoResultController = TextEditingController();
+  final searchTextController = TextEditingController();
+  final searchTextResultController = TextEditingController();
+  final searchAroundController = TextEditingController();
+  final searchAroundResultController = TextEditingController();
 
-  void onGeocodePressed() async {
-    final response = await service.textSearch(keywords: geoTextController.text);
+  void onSearchTextPressed() async {
+    final response =
+        await service.textSearch(keywords: searchTextController.text);
     setState(() {
-      geoResultController.text = jsonEncode(
+      searchTextResultController.text = jsonEncode(
+        response.pois?.map((poi) => poi.toJson()).toList(),
+      );
+    });
+  }
+
+  void onSearchAroundPressed() async {
+    final response =
+        await service.searchAround(searchAroundController.text);
+    setState(() {
+      searchAroundResultController.text = jsonEncode(
         response.pois?.map((poi) => poi.toJson()).toList(),
       );
     });
@@ -52,7 +65,7 @@ class _SearchPoiServicePageState extends State<SearchPoiServicePage> {
                 margin: const EdgeInsets.only(top: 10),
                 child: TextField(
                   textAlignVertical: TextAlignVertical.center,
-                  controller: geoTextController,
+                  controller: searchTextController,
                   decoration: const InputDecoration(
                     isDense: true,
                     border: OutlineInputBorder(borderSide: BorderSide()),
@@ -62,7 +75,7 @@ class _SearchPoiServicePageState extends State<SearchPoiServicePage> {
               Container(
                 margin: const EdgeInsets.only(top: 10),
                 child: FilledButton(
-                  onPressed: onGeocodePressed,
+                  onPressed: onSearchTextPressed,
                   child: const Text("运行"),
                 ),
               ),
@@ -78,7 +91,49 @@ class _SearchPoiServicePageState extends State<SearchPoiServicePage> {
                       readOnly: true,
                       minLines: 5,
                       maxLines: 5,
-                      controller: geoResultController,
+                      controller: searchTextResultController,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: const Text(
+                  "周边搜索",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.center,
+                  controller: searchAroundController,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    border: OutlineInputBorder(borderSide: BorderSide()),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: FilledButton(
+                  onPressed: onSearchAroundPressed,
+                  child: const Text("运行"),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("结果：", style: TextStyle(fontSize: 18)),
+                    TextField(
+                      decoration:
+                          const InputDecoration(border: OutlineInputBorder()),
+                      readOnly: true,
+                      minLines: 5,
+                      maxLines: 5,
+                      controller: searchAroundResultController,
                     ),
                   ],
                 ),
--- lib/src/services/search_poi.dart
+++ lib/src/services/search_poi.dart
@@ -38,4 +38,49 @@ extension SearchPoiService on WebService {
     final response = await doGet(url, headers: apiHeaders);
     return SearchPoiResponse.fromJson(jsonDecode(response.body));
   }
+
+  /// [周边搜索](https://lbs.amap.com/api/webservice/guide/api-advanced/newpoisearch#t4)
+  /// location: 必填，中心点坐标
+  /// keywords: 地点关键字
+  /// types: 指定地点类型
+  /// radius: 搜索半径,取值范围:0-50000，大于50000时按默认值，单位：米,默认为5000
+  /// sortrule: 排序规则，按距离排序：distance；综合排序：weight，默认为distance
+  /// region: 搜索区划
+  /// city_limit: 指定城市数据召回限制，可选值：true/false，默认为false
+  /// show_fields: 返回结果控制
+  /// page_size: 当前分页展示的数据条数，page_size的取值1-25，默认为10
+  /// page_num: 请求第几分页，page_num的取值1-100，默认为1
+  /// output: 返回结果格式类型，默认格式为json，目前只支持json格式
+  Future<SearchPoiResponse> searchAround(
+      String location, {
+        String? keywords,
+        String? types,
+        int? radius,
+        String? sortRule,
+        String? region,
+        bool? cityLimit,
+        String? showFields,
+        int? pageSize,
+        int? pageNum,
+        String? output,
+      }) async {
+    Uri uri = appendApiPath(this.uri, "/v5/place/around");
+    final params = <String, dynamic>{};
+    params['key'] = apiKey;
+    params["location"] = location;
+    if (keywords != null) params["keywords"] = keywords;
+    if (types != null) params["types"] = types;
+    if (radius != null) params["radius"] = radius;
+    if (sortRule != null) params["sortrule"] = sortRule;
+    if (region != null) params["region"] = region;
+    if (cityLimit != null) params["city_limit"] = cityLimit;
+    if (showFields != null) params["show_fields"] = showFields;
+    if (pageSize != null) params["page_size"] = pageSize;
+    if (pageNum != null) params["page_num"] = pageNum;
+    if (output != null) params["output"] = output;
+    if (secretKey != null) params["sig"] = generateSignature(params);
+    final url = appendParameters(uri, params).toString();
+    final response = await doGet(url, headers: apiHeaders);
+    return SearchPoiResponse.fromJson(jsonDecode(response.body));
+  }
 }
